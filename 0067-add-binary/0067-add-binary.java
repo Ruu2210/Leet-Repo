907c00a509840cf70785ce43b241f7bee73d6810
Rutika Shinde <97535166+Ruu2210@users.noreply.github.com>
2023-02-14 19:45:10 +0530

    Time: 1 ms (100.00%), Space: 41.1 MB (72.85%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,14 +1,15 @@
 public class Solution {
     public String addBinary(String a, String b) {
+        //StringBuilder in Java is a class used to create a mutable, or in other words, a modifiable succession of characters
         StringBuilder sb = new StringBuilder();
         int i = a.length() - 1;
         int j = b.length() -1;
         int carry = 0;
         while (i >= 0 || j >= 0) {
             int sum = carry;
-            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0'; //to convert in integer
             if (i >= 0) sum += a.charAt(i--) - '0';
-            sb.append(sum % 2);
+            sb.append(sum % 2); 
             carry = sum / 2;
         }
         if (carry != 0) sb.append(carry);
