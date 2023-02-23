e668cd87e2dc5339884de52f172ea822e05a9a83
Rutika Shinde <97535166+Ruu2210@users.noreply.github.com>
2023-02-23 23:15:20 +0530

    Time: 121 ms (35.67%), Space: 90.9 MB (12.90%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int projects[][] = new int [n][2]; //Initialize 2D array 
+        int profit=0;
+        for(int i=0;i<n;i++){
+            projects[i][0]= profits[i];
+            projects[i][1]= capital[i];
+        }
+        //Sort projects in increasing order of capital
+        Arrays.sort(projects,(a,b) -> a[1]-b[1]);
+        int i=0;
+        //Initialize priority Queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        while(k>0){
+            
+            while(i<capital.length && projects[i][1]<=w){
+                pq.add(projects[i][0]); //add profits<=w in priorityqueue
+                i++;
+            }
+                if(pq.isEmpty()) {
+                    break;
+                }
+                w=w+pq.poll();
+                k--;
+            }
+        return w;
+        }
+    }
