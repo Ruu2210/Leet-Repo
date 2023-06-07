2ce82fdcaee034a6ff4358bbdc5a0067e0a3a9bf
Rutika Shinde <97535166+Ruu2210@users.noreply.github.com>
2023-06-07 09:51:04 +0530

    Time: 1 ms (99.39%), Space: 44.7 MB (7.65%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<flowerbed.length;i++){
             
             //check current and neighbouring positions are empty or not
-            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1|| flowerbed[i+1]==0)){
                 flowerbed[i]=1; //important to make it 1 so that we can check for next condition
                 count++;
             }
