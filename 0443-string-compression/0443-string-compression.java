f92145e9a8fd2c0082a1a8a398bffcbfb7877537
Rutika Shinde <97535166+Ruu2210@users.noreply.github.com>
2023-06-08 11:42:20 +0530

    Time: 1 ms (96.95%), Space: 43.1 MB (45.41%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,24 +1,31 @@
 class Solution {
     public int compress(char[] chars) {
-        //here we use 2 pointer 
+        //If the group's length is 1, append the character to s.
+        
+        //1)initialize two pointer i and j and count=1;
         int i=0;
-        //at start we consider count=1
-        for(int j=1 ,count =1; j<= chars.length ; j++ ,count++){
-            if(j == chars.length || chars[j]!= chars[j-1]){
-                chars[i]= chars[j-1];
+        
+        for(int j=1 ,count=1; j<=chars.length; j++,count++)
+        {
+            //check if we find 2 different characters
+            if(j== chars.length || chars[j]!= chars[j-1]){
+                //if its true now its time to put character in ith position and after that its count if count>=2
+            chars[i] = chars[j-1];
                 i++;
-            
-            if(count>=2){
-                 String countStr = String.valueOf(count);
-                for (int k = 0; k < countStr.length(); k++) {
-                    chars[i] = countStr.charAt(k);
-                    i++;
-                }
-            
-            
-            }count =0;
+                
+                if(count>=2){
+                    //convert int value of count into string as we need to append string value in array
+                    String countStr = String.valueOf(count);
+                    for(int k =0; k<countStr.length(); k++){
+                        chars[i]=countStr.charAt(k);
+                        i++;
+                    }
+            }
+            //now make count=0 for new fresh count
+                count =0;
         }
-        }
-        return i;
     }
+        //i is our length of new array
+        return i ;
+  }
 }
