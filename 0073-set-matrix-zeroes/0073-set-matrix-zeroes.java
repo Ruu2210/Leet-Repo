6c3ff2299c9a89ad94f236d308603119914a5407
Rutika Shinde <97535166+Ruu2210@users.noreply.github.com>
2023-06-21 21:48:55 +0530

    Time: 1 ms (74.80%), Space: 44.7 MB (36.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -6,9 +6,14 @@ class Solution {
         int col0 =1;  //matrix[0][0] take twice to solve these problem take this extra variable
         
         //transverse the matrix and mark proper cells of 0th row and 0th column
-        for(int i = 0; i<rows; i++){ //n=row
+        for(int i = 0; i<rows; i++){ 
+            //if value at any 0th column is 0 then update col0=0
             if(matrix[i][0] ==0) col0=0;
+            
+            //travel remaning array
             for(int j = 1; j<column; j++){ //m=column 
+                
+                //if current=0 then update selected col and row of that matrix[i][j]
                 if(matrix[i][j] == 0) {
                     matrix[i][0] = matrix[0][j] = 0;
                 }
@@ -16,15 +21,16 @@ class Solution {
         }
         
         
-       //according to 0th row and 0th column mark all other 
+       //Travels from backward  
         for(int i = rows-1; i>=0; i--){
             for(int j = column-1; j>=1; j--){
-                //if in given matrix[i][j] any 0th row or column is 0 mark given place=0
+                //travels from last to 1st bcoz 0th column is depend on col0 variable
+                //check if dummy value of 0th row and 0th column of current position 
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
-                
             }
+            //for 0th column check col0 variable 
             if (col0 == 0) {
                 matrix[i][0] = 0; // ith row and 0th column=0 as matrix[0][0]=0 
               }
